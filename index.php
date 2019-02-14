53e6731a6ce3dc2771d4e43b6b3922139889a348
Akshat Soni <akshatsoni64@gmail.com>
2019-02-14 20:29:43 +0530

    Commit #1

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link rel="icon" type="images/x-icon"href="PICS/logo.jpg">
+        <title>WHERE2PARK</title>
+        <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+        <script src="JS/script.js"></script>
+    </head>
+    <body>
+    
+    </body>
+</html>
